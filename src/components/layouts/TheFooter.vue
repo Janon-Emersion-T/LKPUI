b5d1054711e6c03eb86819296d44274c24b3b0e5
Janon-Emersion-T <janonemersion@hotmail.com>
2024-12-28 12:14:34 +0530

    initial commit

---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="flex justify-between mt-4 px-8 py-4 text-gray-500 border-t-2 text-lg font-bold bg-white">
+    <div>
+      <p>Developed by <a href="https://lkprofessionals.com/">LKProfessionals</a></p>
+    </div>
+    <div></div>
+    <div>&copy; {{ new Date().getFullYear() }} LKP-UI</div>
+    <div>V-1.0</div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
